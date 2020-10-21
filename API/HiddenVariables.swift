500f6ca4c7885cccf8d083e5776a456ada241e04
Will Cunane <willcunane1@gmail.com>
2020-10-21 14:46:31 -0400

    commit

--- API/HiddenVariables.swift
+++ API/HiddenVariables.swift
@@ -10,8 +10,8 @@ import Foundation
 
 /// To congfigure the variables to use go to - https://developer.yahoo.com/weather/ and create an account
 
-let privateAppID : String = ""
-let privateClientSecret : String = ""
-let privateClientID : String = ""
-let privateURL : String = ""
-let privateHeader : String = ""
+let privateAppID : String = "E72OiT32"
+let privateClientSecret : String = "66cc6108aa75af29366049dc0617b82a387670ea"
+let privateClientID : String = "dj0yJmk9SWlHdExuMXJMcmRjJmQ9WVdrOVJUY3lUMmxVTXpJbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PThl"
+let privateURL : String = "https://weather-ydn-yql.media.yahoo.com/forecastrss"
+let privateHeader : String = "E72OiT32"
--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 [Yahoo Weather](https://developer.yahoo.com/weather/)
 
 and follow the instructions on how to obtain an API key along with the client and app ID's. Place them inside the variables in the `HiddenVariables.swift` file.
+
+![Image of Weather App](https://willcunane.com/img/weather.png)
